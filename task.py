f8f7575e55bc158da14292f00bd1503d13990d7e
Matthew Slipenchuk <mslipenchuk267@gmail.com>
2018-09-14 14:02:21 -0400

    deleted un-needed image and general debugging

--- task.py
+++ task.py
@@ -7,31 +7,33 @@ import datetime
 import random
 
 # Path to working directory
+timer = core.Clock()
 directory = os.getcwd()
 
 # Timings
 itiDuration = 1
-decisionDuration = 5
+decisionDuration = 3
+postChoiceDuration = 1
 tryFasterDuration = 1
 thankYouScreenDuration = 1.5
 
 # Trial data initlization
 numTrials = 10
-trialType = [''] * len(numTrials) # 1 denotes money and machine selection game
+trialType = [''] * numTrials # 1 denotes money and machine selection game
                                   # 2 denotes machine and machine selection game
 moneyTypes = ['$5','$10','$50']
 percentageTypes = ['25%', '50%', '75%', '??']
 
 # Data Logging
 key = ['']
-responses = [''] * len(numTrials)
-leftMachineTypes = [''] * len(numTrials) # Image names
-rightMachineTypes = [''] * len(numTrials)
-leftMachinePercentages = [''] * len(numTrials) # Percentages displayed in machine
-rightMachinePercentages = [''] * len(numTrials)
-leftMachineMoneyAmounts =[''] * len(numTrials) # Money Amount offered above machines
-rightMachineMoneyAmounts = [''] * len(numTrials)
-moneyOptions = [''] * len(numTrials) # Money option on the left
+responses = [''] * numTrials
+leftMachineTypes = [''] * numTrials # Image names
+rightMachineTypes = [''] * numTrials
+leftMachinePercentages = [''] * numTrials # Percentages displayed in machine
+rightMachinePercentages = [''] * numTrials
+leftMachineMoneyAmounts =[''] * numTrials # Money Amount offered above machines
+rightMachineMoneyAmounts = [''] * numTrials
+moneyOptions = [''] * numTrials # Money option on the left
 
 # Get subjID
 subjDlg = gui.Dlg(title="JOCN paper - rate items")
@@ -46,10 +48,10 @@ if len(subj_id) < 1: # Make sure participant entered name
 win = visual.Window(fullscr=True, size=[1200, 800], units='pix', monitor='testMonitor', color = [-.9,-.9,-.9])
 
 # Initalize Text Stim
-instruction_screen = visual.TextStim(win, text="""To select the option presented on the left press ‘f’.\n
-                                            To select the option presented on the right press ‘g’.\n
-                                            You will have 6 seconds to make your choice.\n\n
-                                            Press any key to start""")
+instruction_screen = visual.TextStim(win, text='''To select the option presented on the left press 'f'.\n
+                                                   To select the option presented on the right press 'g'.\n
+                                                   You will have 6 seconds to make your choice.\n\n
+                                                   Press any key to start''' )
 thank_you_screen = visual.TextStim(win, text="""Thank you for playing!""")
 try_faster_screen = visual.TextStim(win, text='Please make a faster decision next round!')
 iti = visual.TextStim(win, text="""*""")
@@ -64,8 +66,8 @@ imageList = [os.path.join(directory, image) for image in
                  glob.glob('images/*.jpg')]
 
 # Initalize Image Stim
-leftMachine = visual.ImageStim(win=win, image=image[0], units='pix', pos=[0, 200], size = [300,300])
-rightMachine = visual.ImageStim(win=win, image=image[0], units='pix', pos=[0, 200], size = [300,300])
+leftMachine = visual.ImageStim(win=win, image=imageList[0], units='pix', pos=[0, 200], size = [300,300])
+rightMachine = visual.ImageStim(win=win, image=imageList[0], units='pix', pos=[0, 200], size = [300,300])
 
 # Show instruction screen
 event.clearEvents()
@@ -80,8 +82,8 @@ if 'escape' in event.waitKeys():
 for i in range(0, numTrials):
     # Set stim parameters and log data
     if trialType[i] == 1: # 1 indicates money option and slot machine game
-        rightMachineImage = imageList[[random.randint(0,len(imageList)-1)]]
-        rightMachinePercentage = percentageTypes[[random.randint(0,len(percentageTypes)-1)]]
+        rightMachineImage = imageList[random.randint(0,len(imageList)-1)]
+        rightMachinePercentage = percentageTypes[random.randint(0,len(percentageTypes)-1)]
         rightMachineMoneyAmount = moneyTypes[random.randint(0,len(moneyTypes)-1)]
         moneyOption = moneyTypes[random.randint(0,len(moneyTypes)-1)]
         moneyOptionChoice.setText(moneyOption)
@@ -92,12 +94,12 @@ for i in range(0, numTrials):
         rightMachineTypes[i] = rightMachineImage
         leftMachinePercentages[i] = 'n/a'
         rightMachinePercentages[i] = rightMachinePercentage
-        leftMachineMoneyAmounts = 'n/a'
-        rightMachineMoneyAmounts = rightMachineMoneyAmount
+        leftMachineMoneyAmounts[i] = 'n/a'
+        rightMachineMoneyAmounts[i] = rightMachineMoneyAmount
         moneyOptions[i] = moneyOption
     else: # slot machine and slot machine game
-        leftMachineImage = imageList[[random.randint(0,len(imageList)-1)]]
-        rightMachineImage = imageList[[random.randint(0,len(imageList)-1)]]
+        leftMachineImage = imageList[random.randint(0,len(imageList)-1)]
+        rightMachineImage = imageList[random.randint(0,len(imageList)-1)]
         leftMachineMoneyAmount = moneyTypes[random.randint(0,len(moneyTypes)-1)]
         rightMachineMoneyAmount = moneyTypes[random.randint(0,len(moneyTypes)-1)]
         leftMachinePercentage = percentageTypes[random.randint(0,len(percentageTypes)-1)]
@@ -112,19 +114,46 @@ for i in range(0, numTrials):
         rightMachineTypes[i] = rightMachineImage
         leftMachinePercentages[i] = leftMachinePercentage
         rightMachinePercentages[i] = rightMachinePercentage
-        leftMachineMoneyAmounts = leftMachineMoneyAmount
-        rightMachineMoneyAmounts = rightMachineMoneyAmount
+        leftMachineMoneyAmounts[i] = leftMachineMoneyAmount
+        rightMachineMoneyAmounts[i] = rightMachineMoneyAmount
         moneyOptions[i] = 'n/a'
 
     event.clearEvents() # If participant presses key(s) during iti
     timer.reset()
     while timer.getTime() < decisionDuration:
+        # Detect Subject's key press and reference value with var key
+        key = event.waitKeys()
+        if key[0] in ['f','g']:
+            if trialType[i] == 1:
+                moneyOptionChoice.draw()
+                moneyOptionRightMachine.draw()
+                rightMachine.draw()
+                percentageRight.draw()
+                win.flip()
+            else:
+                moneyOptionLeftMachine.draw()
+                leftMachine.draw()
+                percentageLeft.draw()
+                moneyOptionRightMachine.draw()
+                rightMachine.draw()
+                percentageRight.draw()
+                win.flip()
+            responses[i] = key[0]
+            core.wait(postChoiceDuration)
+            iti.draw()
+            win.flip()
+            core.wait(itiDuration)
+            break # Ends trial before decisionDuration, starts new trial
+        elif key[0] in ['escape','esc']:
+            core.quit()
+        
         # Draw Image and Text Stim respective to trial type
         if trialType[i] == 1:
             moneyOptionChoice.draw()
             moneyOptionRightMachine.draw()
             rightMachine.draw()
             percentageRight.draw()
+            win.flip()
         else:
             moneyOptionLeftMachine.draw()
             leftMachine.draw()
@@ -132,16 +161,9 @@ for i in range(0, numTrials):
             moneyOptionRightMachine.draw()
             rightMachine.draw()
             percentageRight.draw()
+            win.flip()
 
-        # Detect Subject's key press and reference value with var key
-        key = event.waitKeys()
-        if key[0] in ['f','g']:
-            responses[i] = key[0]
-            break # Ends trial before decisionDuration, starts new trial
-        if 'escape' in event.waitKeys(): # Allows subject to leave game
-            core.quit()
-
-    if responses[i] = '':
+    if responses[i] == '':
         responses[i] = 'n/a'
         event.clearEvents()
         try_faster_screen.draw()
